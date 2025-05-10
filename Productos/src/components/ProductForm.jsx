b16d77ef5a6183d31975c33640dfdb63a3d56b98
Ivo <ivothaielvicencio@gmail.com>
2025-05-10 12:26:13 -0300

    feature: Creación de un ejemplo del tp con similitudes en ejercicios para tener una guia de ejemplo [Ivo Vicencio]

---
+++ Productos/src/components/ProductForm.jsx
@@ -0,0 +1,10 @@
+function ProductForm() {
+    const [producto, setProducto] = useState({
+        id: "",
+        descripcion: "",
+        precioUnitario: "",
+        descuento: "",
+        stock: "",
+    });
+    const [productos, setProductos] = useState([]);
+}
